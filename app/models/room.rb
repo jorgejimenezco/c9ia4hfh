dd142b7dc2f3d6bbbac7fbd0dad5adc16af4e54e
Jorge Jimenez <stellajimenez@Apple-Mac.local>
2017-03-21 20:06:00 -0500

    add numericality

--- app/models/room.rb
+++ app/models/room.rb
@@ -14,7 +14,7 @@
 
 class Room < ActiveRecord::Base
   validates :title, :description, :beds, :guests, presence: true
-  validates :description, length: { in: 5...400 }
-  validates :beds, :only_integer
-  validates :guests, :only_integer
+  validates :description, length: { maximum: 400 }
+  validates :beds, numericality: { only_integer: true }
+  validates :guests, numericality: { only_integer: true }
 end
